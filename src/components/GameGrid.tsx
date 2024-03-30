bee332fdafd3b2e7025700df34f244ae2987866c
01XASSASSIN <hariharanuv7@gmail.com>
2024-03-30 12:52:29 +0530

    Displaying Platform Icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,6 @@ import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
-  console.log(games);
   return (
     <>
       {error && <Text>{error}</Text>}
